1de21793341eef6e153a257d85d4419faf5034b0
Taegwan Youn <tkyoun0421@naver.com>
2025-06-11 16:55:27 +0900

    chore: 토큰 오류시 메인 화면으로 이동

--- src/features/playlist/hooks/useGetPlaylist.ts
+++ src/features/playlist/hooks/useGetPlaylist.ts
@@ -2,18 +2,24 @@ import { useQuery } from "@tanstack/react-query";
 import { UseQueryResult } from "@tanstack/react-query";
 import { useParams } from "react-router";
 
+import { useTokenStore } from "@features/auth/store/useTokenStore";
 import getPlaylist from "@features/playlist/api/getPlaylist";
 import { Playlist } from "@features/playlist/model/playlist";
 
-const useGetPlaylist = (): UseQueryResult<Playlist> => {
+const useGetPlaylist = (): UseQueryResult<Playlist, Error> => {
+  const { access_token } = useTokenStore();
   const { id } = useParams();
-  return useQuery({
-    queryKey: ["playlist", id],
+
+  return useQuery<Playlist, Error>({
+    queryKey: ["playlist", id, access_token],
     queryFn: () => {
-      if (!id) throw new Error("id is required");
+      if (!id) throw new Error("Playlist ID is required");
+      if (!access_token) throw new Error("Access token is required");
       return getPlaylist(id);
     },
-    enabled: !!id,
+    enabled: !!id && !!access_token,
+    retry: false, // 인증 오류 시 재시도 방지
   });
 };
+
 export default useGetPlaylist;
